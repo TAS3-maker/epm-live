93024d9a46d5207fc44eaefd9eb7aab06161e203
TAS3-maker <dm@techarchsoftwares.com>
2025-06-18 18:03:52 +0530

    run build

--- epm-rip-main/src/components/utils/ApiConfig.js
+++ epm-rip-main/src/components/utils/ApiConfig.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'http://13.51.178.43/api';
-export const API_URL = 'http://127.0.0.1:8000';
+export const API_URL = 'http://13.60.21.239/api/';
+// export const API_URL = 'http://127.0.0.1:8000';
